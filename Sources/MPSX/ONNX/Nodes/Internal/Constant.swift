3cb2fcfd912c8c64dbd77ec19d82116a0fc76535
Pavel Serdziukou <iamshvul@gmail.com>
2023-03-07 16:54:01 +0300

    Int values read fixes for onnx protobuf
    Different values might be stored in different place within swift protobuf for onnx models

--- Sources/MPSX/ONNX/Nodes/Internal/Constant.swift
+++ Sources/MPSX/ONNX/Nodes/Internal/Constant.swift
@@ -9,13 +9,37 @@ extension MPSGraph {
         case (.float16, .float16),
              (.float32, .float):
             return constant(tensor.rawData, shape: tensor.mpsShape, dataType: targetDataType)
+        case (.float16, .int64):
+            if tensor.rawData.count != 0 {
+                guard let values = tensor.halfs else {
+                    throw OnnxError.unsupportedTensorDataType(onnx: tensor.dataType, mps: nil)
+                }
+                return constant(values.rawData, shape: tensor.mpsShape, dataType: targetDataType)
+            } else {
+                guard !tensor.int64Data.isEmpty else {
+                    throw OnnxError.unsupportedTensorDataType(onnx: tensor.dataType, mps: nil)
+                }
+                return constant(tensor.int64Data.rawData, shape: tensor.mpsShape, dataType: targetDataType)
+            }
+        case (.float32, .int64):
+            if tensor.rawData.count != 0 {
+                guard let values = tensor.floats else {
+                    throw OnnxError.unsupportedTensorDataType(onnx: tensor.dataType, mps: nil)
+                }
+                return constant(values.rawData, shape: tensor.mpsShape, dataType: targetDataType)
+            } else {
+                guard !tensor.int64Data.isEmpty else {
+                    throw OnnxError.unsupportedTensorDataType(onnx: tensor.dataType, mps: nil)
+                }
+                return constant(tensor.int64Data.rawData, shape: tensor.mpsShape, dataType: targetDataType)
+            }
         case (.float16, _):
-            guard let values = tensor.halfs else {
+            guard let values = tensor.halfs, values.rawData.count != 0 else {
                 throw OnnxError.unsupportedTensorDataType(onnx: tensor.dataType, mps: nil)
             }
             return constant(values.rawData, shape: tensor.mpsShape, dataType: targetDataType)
         case (.float32, _):
-            guard let values = tensor.floats else {
+            guard let values = tensor.floats, values.rawData.count != 0 else {
                 throw OnnxError.unsupportedTensorDataType(onnx: tensor.dataType, mps: nil)
             }
             return constant(values.rawData, shape: tensor.mpsShape, dataType: targetDataType)
