0f2133c5440c6fe7474c6af2f18456ce4f30aa93
Pavel Serdziukou <iamshvul@gmail.com>
2023-04-07 15:02:51 +0300

    Slice fix (#11)

--- Sources/MPSX/ONNX/Nodes/Slice.swift
+++ Sources/MPSX/ONNX/Nodes/Slice.swift
@@ -7,21 +7,44 @@ extension MPSGraph {
         _ tensors: [String: MPSGraphTensor],
         _ constants: [String: Onnx_TensorProto]
     ) throws -> MPSGraphTensor {
-        guard node.input.count == 3,
-              let input = tensors(node.input(0)),
+        guard let input = tensors(node.input(0)),
+              let shape = input.shape,
               let starts = constants(node.input(1))?.ints,
-              let ends = constants(node.input(2))?.ints,
-              starts.count == ends.count
+              let ends = constants(node.input(2))?.ints
         else { throw OnnxError.invalidInput(node.name) }
-
+        
+        let steps = constants(node.input(4))?.ints
+        
+        var shapedStart = [Int]()
+        var shapedEnds = [Int]()
+        var shapedStrides = [Int]()
+        if let axes = constants(node.input(3))?.ints {
+            // manually filling missed dimensions
+            for (dim, count) in shape.enumerated() {
+                if let index = axes.firstIndex(where: { $0 == dim }) {
+                    shapedStart.append(starts[index])
+                    shapedEnds.append(Swift.min(ends[index], count.intValue))
+                    shapedStrides.append(steps?[index] ?? 1)
+                } else {
+                    shapedStart.append(0)
+                    shapedEnds.append(count.intValue)
+                    shapedStrides.append(1)
+                }
+            }
+        } else {
+            shapedStart = starts
+            shapedEnds = ends
+            shapedStrides = steps ?? .init(repeating: 1, count: starts.count)
+        }
+        
         let output = sliceTensor(
             input,
-            starts: starts.nsnumbers,
-            ends: ends.nsnumbers,
-            strides: .init(repeating: 1, count: starts.count),
+            starts: shapedStart.nsnumbers,
+            ends: shapedEnds.nsnumbers,
+            strides: shapedStrides.nsnumbers,
             name: nil
         )
-
+        
         return output
     }
 }
